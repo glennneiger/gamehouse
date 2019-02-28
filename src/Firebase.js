6d22362705be6fa8b46e6bc98914d12976fc396d
Jacob Garcia <jacobgarcia@aggienetwork.com>
2019-02-27 18:04:36 -0600

    firebase config

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,19 @@
+
 import * as firebase from 'firebase';
 
 import config from './config/keys';
+
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://jacobsgamehouse.firebaseio.com"
+});
+
+
 
 firebase.initializeApp(config);
 export const database = firebase.database();
---
+++ src/config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  apiKey: "AIzaSyDQjtiZ2Z-uge24UdgzOzBPDocn54kQDSc",
+  authDomain: "jacobsgamehouse.firebaseapp.com",
+  databaseURL: "https://jacobsgamehouse.firebaseio.com",
+  projectId: "jacobsgamehouse",
+  storageBucket: "jacobsgamehouse.appspot.com",
+  messagingSenderId: "529121111976"
+
+}
