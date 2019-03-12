ab1c2a058f714372b46aefbbcc9ca43057a0165d
Jacob Garcia <jacobgarcia@aggienetwork.com>
2019-03-12 17:08:33 -0500

    grid now handles any number of players being added or removed simultaneously

--- src/components/computer/PlayerGrid.js
+++ src/components/computer/PlayerGrid.js
@@ -11,30 +11,72 @@ class PlayerGrid extends Component {
   }
 
   componentDidUpdate() {
+
+    const oldPlayers = this.state.players;
     const {players} = this.props;
-    let {cards} = this.state;
-    if (players.length !== cards.length) {
-      if (players.length > cards.length) {
-        const i = players.length-1;
-        const newPlayer = players[i];
-        const key = players[i].index;
+
+    if (players.length !== oldPlayers.length) {
+      if (players.length > oldPlayers.length) {
+        const playersToAdd = players.slice(oldPlayers.length, players.length);
+        this.addCards(playersToAdd);
+      } else {
+        let indicesOfPlayersToKeep = players.map(player=>player.index);
+        let playersToRemove = oldPlayers.filter(player => !indicesOfPlayersToKeep.includes(player.index));
+        this.removeCards(playersToRemove);
+      }
+      this.setState({players});
+    }
+  }
+
+  addCards = playersToAdd=> {
+    let {players} = this.state;
+
+    let cards = []; 
+    // get cards from state.players, not from state.cards. state.cards might not reflect players that have been removed but are still in the process of being animated out. Therefore, state.players is more up-to-date
+    players.forEach(player=>{
+      const key = player.index;
+      cards.push(
+        <div className="flex-item new-item" key={key} id={`flex-${key}`} >
+          <Card name={player.name} img={player.img} />
+        </div>
+      )
+    });
+
+    let interval = 10;
+    playersToAdd.forEach(player => {
+      const key = player.index;
+      // add one at a time with slight intervals between for a smoother effect
+      setTimeout(()=>{
         cards.push(
           <div className="flex-item new-item" key={key} id={`flex-${key}`} >
-            <Card name={newPlayer.name} img={newPlayer.img} />
+            <Card name={player.name} img={player.img} />
           </div>
-        )
-        this.setState({cards, players})
-      } else {
-        const filtered = this.state.players.filter(player => players.indexOf(player)<0);
-        const i = filtered[0].index; // should only remove one at a time
+        )    
+        this.setState({cards});
+      }, interval);
+      interval += 150;
+    });
+  }
+
+  removeCards = playersToRemove=> {
+    let interval = 10;
+    playersToRemove.forEach(player=>{
+      const i = player.index; 
+      setTimeout(()=>{
         let item = document.getElementById(`flex-${i}`);
         item.classList.add('remove-item');
-        item.addEventListener('animationend', ()=> { //  webkitAnimationEnd oAnimationEnd MSAnimationEnd
-          cards = cards.filter(card=> parseInt(card.key) !== i);
-          this.setState({cards, players})
-        });
-      }
-    }
+        item.addEventListener('animationend', ()=> {
+          this.clearCard(i);
+        })
+      }, interval);
+      interval += 150;
+    });
+  }
+
+  clearCard = index=> {
+    let {cards} = this.state;
+    cards = cards.filter(card=> parseInt(card.key) !== index);
+    this.setState({cards})
   }
 
   renderCards = ()=> {
